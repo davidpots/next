4e0a10872ab6a7d0c5dc6463047812e455e30a1a
David Potsiadlo <davidpots@gmail.com>
2015-11-14 15:14:41 -0600

    sigh

--- 2/javascript/test.js
+++ 2/javascript/test.js
@@ -14,101 +14,107 @@ var myAudio,
     bufferedEnd,
     audioStartTime;
 
-function initPlayer(audio) {
-
-
-
-}
+// function initPlayer(audio) {
+//
+//
+//
+// }
 
 $(document).ready(function(){
 
-
-
   var myAudio = document.getElementById('my-audio');
-  // myAudio.load();
-  // if (myAudio) {
-    // initPlayer(myAudio);
-    myAudio.load();
-
-    myAudio.play();
-    myAudio.pause();
-
-    myAudio.currentTime = 1000;
-    // myAudio.addEventListener('canplay', function () {
-      // myAudio.play();
-    // });
-  // }
 
 
-  $('.playerUI-play').click(function(){
-    myAudio.pause();
-    myAudio.currentTime = 1000;
-    myAudio.play();
+  myAudio.currentTime = 5;
+  myAudio.play();
 
 
-
-    return false;
-  });
-
-
-
-  // }
 //
-//   // Load the audio
-//   myAudio.load();
-//   duration = myAudio.duration;
+//   var myAudio = document.getElementById('my-audio');
+//   // myAudio.load();
+//   // if (myAudio) {
+//     // initPlayer(myAudio);
+//     myAudio.load();
 //
-//   // Retrieve the desired start time for the audio
-//   audioStartTime = 1000;
+//     myAudio.play();
+//     myAudio.pause();
 //
-//   myAudio.oncanplay = function() {
 //     myAudio.currentTime = 1000;
-//   }
+//     // myAudio.addEventListener('canplay', function () {
+//       // myAudio.play();
+//     // });
+//   // }
 //
 //
-//     // myAudio.currentTime = 1000;
-// // myAudio.play();
-// // myAudio.pause();
+//   $('.playerUI-play').click(function(){
+//     myAudio.pause();
+//     myAudio.currentTime = 1000;
+//     myAudio.play();
 //
 //
 //
-
-
-
-  // setInterval(function() {
-  //
-  //   if ( myAudio.buffered.length >= 1  ) {
-  //     duration = myAudio.duration;
-  //     var bufferedEnd = myAudio.buffered.end(myAudio.buffered.length - 1);
-  //     document.getElementById('buffered-amount').style.width = ((bufferedEnd / duration)*100) + "%";
-  //   }
-  // }, 1000);
-
-
-
-
-
-
-
-  // myAudio.addEventListener('progress', function() {
-  //   if ( myAudio.buffered.length >= 1  ) {
-  //     var bufferedEnd = myAudio.buffered.end(myAudio.buffered.length - 1);
-  //   }
-  //   var duration =  myAudio.duration;
-  //   if (duration > 0) {
-  //     document.getElementById('buffered-amount').style.width = ((bufferedEnd / duration)*100) + "%";
-  //   }
-  // });
-  //
-
-
-
-
-  // myAudio.addEventListener('timeupdate', function() {
-  //   var duration =  myAudio.duration;
-  //   if (duration > 0) {
-  //     document.getElementById('progress-amount').style.width = ((myAudio.currentTime / duration)*100) + "%";
-  //   }
-  // });
-
+//     return false;
+//   });
+//
+//
+//
+//   // }
+// //
+// //   // Load the audio
+// //   myAudio.load();
+// //   duration = myAudio.duration;
+// //
+// //   // Retrieve the desired start time for the audio
+// //   audioStartTime = 1000;
+// //
+// //   myAudio.oncanplay = function() {
+// //     myAudio.currentTime = 1000;
+// //   }
+// //
+// //
+// //     // myAudio.currentTime = 1000;
+// // // myAudio.play();
+// // // myAudio.pause();
+// //
+// //
+// //
+//
+//
+//
+//   // setInterval(function() {
+//   //
+//   //   if ( myAudio.buffered.length >= 1  ) {
+//   //     duration = myAudio.duration;
+//   //     var bufferedEnd = myAudio.buffered.end(myAudio.buffered.length - 1);
+//   //     document.getElementById('buffered-amount').style.width = ((bufferedEnd / duration)*100) + "%";
+//   //   }
+//   // }, 1000);
+//
+//
+//
+//
+//
+//
+//
+//   // myAudio.addEventListener('progress', function() {
+//   //   if ( myAudio.buffered.length >= 1  ) {
+//   //     var bufferedEnd = myAudio.buffered.end(myAudio.buffered.length - 1);
+//   //   }
+//   //   var duration =  myAudio.duration;
+//   //   if (duration > 0) {
+//   //     document.getElementById('buffered-amount').style.width = ((bufferedEnd / duration)*100) + "%";
+//   //   }
+//   // });
+//   //
+//
+//
+//
+//
+//   // myAudio.addEventListener('timeupdate', function() {
+//   //   var duration =  myAudio.duration;
+//   //   if (duration > 0) {
+//   //     document.getElementById('progress-amount').style.width = ((myAudio.currentTime / duration)*100) + "%";
+//   //   }
+//   // });
+//
 });
