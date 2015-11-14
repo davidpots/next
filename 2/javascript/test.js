d5cb30a611407da2c0f748168a8e847e5c21019f
David Potsiadlo <davidpots@gmail.com>
2015-11-14 15:17:07 -0600

    wasalia

--- 2/javascript/test.js
+++ 2/javascript/test.js
@@ -1,120 +1,141 @@
-// var a;
-//
-// $(document).ready(function(){
-//   // a = $('.alpha').get(0);
-//   a.load();
-//   a.currentTime = 2441;
-//   a.play();
-// });
-
-// Declare variables
-var myAudio,
-    currentTime,
-    duration,
-    bufferedEnd,
-    audioStartTime;
-
-// function initPlayer(audio) {
-//
-//
-//
-// }
-
-$(document).ready(function(){
+window.onload = function(){
 
   var myAudio = document.getElementById('my-audio');
+  var play = document.getElementById('play');
+  var pause = document.getElementById('pause');
+
+  // associate functions with the 'onclick' events
+  play.onclick = playAudio;
+  pause.onclick = pauseAudio;
+
+  function playAudio() {
+    myAudio.play();
+  }
+
+  function pauseAudio() {
+    myAudio.pause();
+  }
+
+}
 
 
-  myAudio.currentTime = 5;
-  myAudio.play();
-
-
+// // var a;
+// //
+// // $(document).ready(function(){
+// //   // a = $('.alpha').get(0);
+// //   a.load();
+// //   a.currentTime = 2441;
+// //   a.play();
+// // });
+//
+// // Declare variables
+// var myAudio,
+//     currentTime,
+//     duration,
+//     bufferedEnd,
+//     audioStartTime;
+//
+// // function initPlayer(audio) {
+// //
+// //
+// //
+// // }
+//
+// $(document).ready(function(){
 //
 //   var myAudio = document.getElementById('my-audio');
-//   // myAudio.load();
-//   // if (myAudio) {
-//     // initPlayer(myAudio);
-//     myAudio.load();
-//
-//     myAudio.play();
-//     myAudio.pause();
-//
-//     myAudio.currentTime = 1000;
-//     // myAudio.addEventListener('canplay', function () {
-//       // myAudio.play();
-//     // });
-//   // }
 //
 //
-//   $('.playerUI-play').click(function(){
-//     myAudio.pause();
-//     myAudio.currentTime = 1000;
-//     myAudio.play();
+//   myAudio.currentTime = 5;
+//   myAudio.play();
 //
 //
-//
-//     return false;
-//   });
-//
-//
-//
-//   // }
 // //
-// //   // Load the audio
-// //   myAudio.load();
-// //   duration = myAudio.duration;
+// //   var myAudio = document.getElementById('my-audio');
+// //   // myAudio.load();
+// //   // if (myAudio) {
+// //     // initPlayer(myAudio);
+// //     myAudio.load();
 // //
-// //   // Retrieve the desired start time for the audio
-// //   audioStartTime = 1000;
+// //     myAudio.play();
+// //     myAudio.pause();
 // //
-// //   myAudio.oncanplay = function() {
 // //     myAudio.currentTime = 1000;
-// //   }
+// //     // myAudio.addEventListener('canplay', function () {
+// //       // myAudio.play();
+// //     // });
+// //   // }
 // //
 // //
-// //     // myAudio.currentTime = 1000;
-// // // myAudio.play();
-// // // myAudio.pause();
+// //   $('.playerUI-play').click(function(){
+// //     myAudio.pause();
+// //     myAudio.currentTime = 1000;
+// //     myAudio.play();
 // //
 // //
 // //
-//
-//
-//
-//   // setInterval(function() {
-//   //
-//   //   if ( myAudio.buffered.length >= 1  ) {
-//   //     duration = myAudio.duration;
-//   //     var bufferedEnd = myAudio.buffered.end(myAudio.buffered.length - 1);
-//   //     document.getElementById('buffered-amount').style.width = ((bufferedEnd / duration)*100) + "%";
-//   //   }
-//   // }, 1000);
-//
-//
-//
-//
-//
-//
-//
-//   // myAudio.addEventListener('progress', function() {
-//   //   if ( myAudio.buffered.length >= 1  ) {
-//   //     var bufferedEnd = myAudio.buffered.end(myAudio.buffered.length - 1);
-//   //   }
-//   //   var duration =  myAudio.duration;
-//   //   if (duration > 0) {
-//   //     document.getElementById('buffered-amount').style.width = ((bufferedEnd / duration)*100) + "%";
-//   //   }
-//   // });
-//   //
-//
-//
-//
-//
-//   // myAudio.addEventListener('timeupdate', function() {
-//   //   var duration =  myAudio.duration;
-//   //   if (duration > 0) {
-//   //     document.getElementById('progress-amount').style.width = ((myAudio.currentTime / duration)*100) + "%";
-//   //   }
-//   // });
-//
-});
+// //     return false;
+// //   });
+// //
+// //
+// //
+// //   // }
+// // //
+// // //   // Load the audio
+// // //   myAudio.load();
+// // //   duration = myAudio.duration;
+// // //
+// // //   // Retrieve the desired start time for the audio
+// // //   audioStartTime = 1000;
+// // //
+// // //   myAudio.oncanplay = function() {
+// // //     myAudio.currentTime = 1000;
+// // //   }
+// // //
+// // //
+// // //     // myAudio.currentTime = 1000;
+// // // // myAudio.play();
+// // // // myAudio.pause();
+// // //
+// // //
+// // //
+// //
+// //
+// //
+// //   // setInterval(function() {
+// //   //
+// //   //   if ( myAudio.buffered.length >= 1  ) {
+// //   //     duration = myAudio.duration;
+// //   //     var bufferedEnd = myAudio.buffered.end(myAudio.buffered.length - 1);
+// //   //     document.getElementById('buffered-amount').style.width = ((bufferedEnd / duration)*100) + "%";
+// //   //   }
+// //   // }, 1000);
+// //
+// //
+// //
+// //
+// //
+// //
+// //
+// //   // myAudio.addEventListener('progress', function() {
+// //   //   if ( myAudio.buffered.length >= 1  ) {
+// //   //     var bufferedEnd = myAudio.buffered.end(myAudio.buffered.length - 1);
+// //   //   }
+// //   //   var duration =  myAudio.duration;
+// //   //   if (duration > 0) {
+// //   //     document.getElementById('buffered-amount').style.width = ((bufferedEnd / duration)*100) + "%";
+// //   //   }
+// //   // });
+// //   //
+// //
+// //
+// //
+// //
+// //   // myAudio.addEventListener('timeupdate', function() {
+// //   //   var duration =  myAudio.duration;
+// //   //   if (duration > 0) {
+// //   //     document.getElementById('progress-amount').style.width = ((myAudio.currentTime / duration)*100) + "%";
+// //   //   }
+// //   // });
+// //
+// });
