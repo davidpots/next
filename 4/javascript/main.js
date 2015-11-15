401199ac9d0dfeacb1e3319e4f33dac7568b06c5
David Potsiadlo <davidpots@gmail.com>
2015-11-15 00:26:21 -0600

    Adds debugger view

--- 4/javascript/main.js
+++ 4/javascript/main.js
@@ -3,9 +3,7 @@ var myAudio;
 window.onload = function(){
 
   myAudio = document.getElementById('alpha');
-  
-  myAudio.play();
-  myAudio.pause();
+  myAudio.load();
     
   $('.audioPlay').click(function(){
       myAudio.currentTime = 10;
@@ -13,12 +11,21 @@ window.onload = function(){
       return false;
   });
 
+
+  // WORKS ON DESKTOP, DOESN'T DO ANYTHING ON MOBILE
+  //
   myAudio.addEventListener('canplay', function(e) {
-    console.log('it can play!!!')
-    $('body').css('backgroundColor','#ffffaa');
+    $('.debug-canplay').text("TRUE");
   });
   
   setInterval(function(){
+
+    $('.debug-currentTime').text(myAudio.currentTime);
+    $('.debug-duration').text(myAudio.duration);
+        
+    if ( myAudio.buffered.length >= 1  ) {
+      $('.debug-buffered').text(myAudio.buffered.end(myAudio.buffered.length-1));
+    };
 
   }, 250);
 }
