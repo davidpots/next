244099a1ea380f8aa945020f03821348d5651a3a
David Potsiadlo <davidpots@gmail.com>
2015-11-16 21:17:56 -0600

    Adds 8

--- 3/javascript/main.js
+++ 3/javascript/main.js
@@ -1,11 +1,52 @@
+var tracks =  [
+              	{
+              	  "podcast"    : "The Joe Rogan Experience",
+              	  "ep_title"   : "Jay Leno",
+              	  "timestamp"  : "8:49",
+              	  "url_img"    : "http://static.libsyn.com/p/assets/0/8/6/8/08688b90de11f9ab/JREiTunesImage.jpg",
+              	  "url_mp3"    : "http://traffic.libsyn.com/joeroganexp/p692.mp3"
+              	},
+              	{
+              	  "podcast"    : "Nerdist",
+              	  "ep_title"   : "Robert Rodriguez",
+              	  "timestamp"  : "8:31",
+              	  "url_img"    : "http://static.libsyn.com/p/assets/3/c/4/5/3c45b257669e76d7/nerdistlogo.jpg",
+              	  "url_mp3"    : "http://www.podtrac.com/pts/redirect.mp3/traffic.libsyn.com/nerdist/Nerdist_718_-_Robert_Rodriguez.mp3"
+              	},
+              	{
+              	  "podcast"    : "Reply All",
+              	  "ep_title"   : "DMV Nation",
+              	  "timestamp"  : "1:32",
+              	  "url_img"    : "http://i1.sndcdn.com/avatars-000116177580-6q9skc-original.jpg",
+              	  "url_mp3"    : "http://www.podtrac.com/pts/redirect.mp3/feeds.soundcloud.com/stream/218017796-replyall-34-dmv-nation.mp3"
+              	},
+              	{
+              	  "podcast"    : "Imaginary Worlds",
+              	  "ep_title"   : "Being Batman (For Now)",
+              	  "timestamp"  : "0:07",
+              	  "url_img"    : "http://static.libsyn.com/p/assets/e/0/8/4/e084f450a5bc024f/Imaginary_Worlds3.jpg",
+              	  "url_mp3"    : "http://traffic.libsyn.com/imaginaryworlds/Being_Batman_For_Now.mp3"
+              	},
+              	{
+              	  "podcast"    : "Reconcilable Differences",
+              	  "ep_title"   : "Devouring Essence (John Siracusa Explains Destiny)",
+              	  "timestamp"  : "42:22",
+              	  "url_img"    : "https://relayfm.s3.amazonaws.com/uploads/broadcast/image/18/broadcast_thumbnail_rd_artwork.png",
+              	  "url_mp3"    : "http://www.podtrac.com/pts/redirect.mp3/traffic.libsyn.com/recdiffs/Reconcilable_Differences_008.mp3"
+              	},
+              	{
+              	  "podcast"    : "Fresh Air",
+              	  "ep_title"   : "Marc Maron's Experience Interviewing President Obama",
+              	  "timestamp"  : "0:23",
+              	  "url_img"    : "http://media.npr.org/images/podcasts/2013/primary/fresh_air.png",
+              	  "url_mp3"    : "http://pd.npr.org/anon.npr-mp3/npr/fa/2015/06/20150622_fa_01.mp3?dl=1"
+              	}
+              ]
+
+
+
 window.onload = function(){
 
-  var myAudio = document.getElementById('alpha');
-    
-  $('.audioPlay').click(function(){
-      myAudio.currentTime = 10;
-      myAudio.play();
-      return false;
-  });
+console.log(tracks);
 
 }
--- 8/javascript/main.js
+++ 8/javascript/main.js
@@ -1,76 +1,65 @@
-var myAudio,myAudioB;
+var trackIndex = 0,
+    tracks =  [
+              	{
+              	  "show"       : "The Joe Rogan Experience",
+              	  "episode"    : "Jay Leno",
+              	  "timestamp"  : "8:49",
+              	  "url_img"    : "http://static.libsyn.com/p/assets/0/8/6/8/08688b90de11f9ab/JREiTunesImage.jpg",
+              	  "url_mp3"    : "http://traffic.libsyn.com/joeroganexp/p692.mp3"
+              	},
+              	{
+              	  "show"       : "Nerdist",
+              	  "episode"    : "Robert Rodriguez",
+              	  "timestamp"  : "8:31",
+              	  "url_img"    : "http://static.libsyn.com/p/assets/3/c/4/5/3c45b257669e76d7/nerdistlogo.jpg",
+              	  "url_mp3"    : "http://www.podtrac.com/pts/redirect.mp3/traffic.libsyn.com/nerdist/Nerdist_718_-_Robert_Rodriguez.mp3"
+              	},
+              	{
+              	  "show"       : "Reply All",
+              	  "episode"    : "DMV Nation",
+              	  "timestamp"  : "1:32",
+              	  "url_img"    : "http://i1.sndcdn.com/avatars-000116177580-6q9skc-original.jpg",
+              	  "url_mp3"    : "http://www.podtrac.com/pts/redirect.mp3/feeds.soundcloud.com/stream/218017796-replyall-34-dmv-nation.mp3"
+              	},
+              	{
+              	  "show"       : "Imaginary Worlds",
+              	  "episode"    : "Being Batman (For Now)",
+              	  "timestamp"  : "0:07",
+              	  "url_img"    : "http://static.libsyn.com/p/assets/e/0/8/4/e084f450a5bc024f/Imaginary_Worlds3.jpg",
+              	  "url_mp3"    : "http://traffic.libsyn.com/imaginaryworlds/Being_Batman_For_Now.mp3"
+              	},
+              	{
+              	  "show"       : "Reconcilable Differences",
+              	  "episode"    : "Devouring Essence (John Siracusa Explains Destiny)",
+              	  "timestamp"  : "42:22",
+              	  "url_img"    : "https://relayfm.s3.amazonaws.com/uploads/broadcast/image/18/broadcast_thumbnail_rd_artwork.png",
+              	  "url_mp3"    : "http://www.podtrac.com/pts/redirect.mp3/traffic.libsyn.com/recdiffs/Reconcilable_Differences_008.mp3"
+              	},
+              	{
+              	  "show"       : "Fresh Air",
+              	  "episode"    : "Marc Maron's Experience Interviewing President Obama",
+              	  "timestamp"  : "0:23",
+              	  "url_img"    : "http://media.npr.org/images/podcasts/2013/primary/fresh_air.png",
+              	  "url_mp3"    : "http://pd.npr.org/anon.npr-mp3/npr/fa/2015/06/20150622_fa_01.mp3?dl=1"
+              	}
+              ]
 
-$(window).bind("load", function() {
 
-  myAudio = document.getElementById('alpha');
-  myAudio.load();
+function loadTrack() {
+  track = tracks[trackIndex];
+  $('.player-show').text(track.show);
+  $('.player-episode').text(track.episode);
+  $('.player-img').attr('src',track.url_img);
+  trackIndex++;
+}
 
-  myAudioB = document.getElementById('beta');
-  myAudioB.load();
-    
-  $('.audioPlay').click(function(){
-      // myAudio.currentTime = 3587;
-      myAudio.currentTime = 2441;
-      myAudio.play();
-      return false;
+window.onload = function(){
+
+  loadTrack();
+
+  $('.player-next').click(function(){
+    loadTrack();
+    return false;
   });
 
-  // -----------------------------------------------------------------------------
-  // Is player ready?
-  // -----------------------------------------------------------------------------
-
-        var isAudioReady = setInterval(function(){
-          if ( !isNaN(parseFloat( myAudio.duration )) ) {
-            audioIsReady = true;
-            $('.audioPlay').show();
-            clearInterval(isAudioReady);
-          } else {
-            audioIsReady = false;
-          }   
-        }, 250);
-
-
-  // -----------------------------------------------------------------------------
-  // Debug
-  // -----------------------------------------------------------------------------
-
-        var debugBoxHTML = "<div class='debug-box'>\
-                              <p><strong>Current time:</strong> <span class='debug-currentTime'>--</span></p>\
-                              <p><strong>Duration:</strong> <span class='debug-duration'>--</span></p>\
-                              <p><strong>Buffered:</strong> <span class='debug-buffered'>--</span></p>\
-                              <p><strong>canplay:</strong> <span class='debug-canplay'>--</span></p>\
-                              <p><strong>canplaythrough:</strong> <span class='debug-canplaythrough'>--</span></p>\
-                            </div>";
-
-        $( ".audioSource audio" ).each(function(i,obj) {
-          setupDebug(obj);
-        });
-
-        function setupDebug(audio) {
-
-          $(debugBoxHTML).insertAfter(audio);
-          
-          var parentDiv = $(audio).parent();
-
-          audio.addEventListener('canplaythrough', function(e) {
-            $(parentDiv).find('.debug-canplaythrough').text("TRUE");
-          });
-
-          audio.addEventListener('canplay', function(e) {
-            $(parentDiv).find('.debug-canplay').text("TRUE");
-          });
-
-          setInterval(function(){
-
-            $(parentDiv).find('.debug-currentTime').text(audio.currentTime);
-            $(parentDiv).find('.debug-duration').text(audio.duration);
-                
-            if ( audio.buffered.length >= 1  ) {
-              $(parentDiv).find('.debug-buffered').text(audio.buffered.end(audio.buffered.length-1));
-            };
-
-          }, 250);
-          
-        }
-
-});
+}
